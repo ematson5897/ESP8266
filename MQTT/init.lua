d3cc6fb1357e9f8aea61cac0542a4fdabf5eb65d
Elliot Matson <ematson5897@gmail.com>
2015-03-18 16:28:11 -0500

    Added switch

--- MQTT/init.lua
+++ MQTT/init.lua
@@ -1,6 +1,14 @@
 
 print("waiting 5 seconds")
 
-tmr.alarm(5,5000,0,function() print("running file") dofile("SensorNode.lua") end)
+tmr.alarm(5,5000,0,function()
+	print("running file")
+	if node.chipid() == 10379351 do
+		dofile("SensorNode.lua") 
+	else
+		dofile("LEDNode.lua") 
+	end
+end)
+	
 
 
