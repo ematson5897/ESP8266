2d29299e26888f1c2da1337f2dd8dfb01c8f4b6f
Elliot Matson <ematson5897@gmail.com>
2015-03-18 16:30:06 -0500

    fix

--- MQTT/init.lua
+++ MQTT/init.lua
@@ -3,9 +3,11 @@ print("waiting 5 seconds")
 
 tmr.alarm(5,5000,0,function()
 	print("running file")
-	if node.chipid() == 10379351 do
+	if node.chipid() == 10379351 then
+		print("sensorNode")
 		dofile("SensorNode.lua") 
 	else
+		print("LEDNode")
 		dofile("LEDNode.lua") 
 	end
 end)
